6aa4200cbf9c343753437f0c9c368038f6359a7a
Kilvis <kyndtkilian2006@gmail.com>
2023-06-03 12:57:51 +0200

    added ui and changed script.js to work with the ui

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function getComputerChoice() {
     computerChoiceInt = Math.ceil(Math.random()*10);
 
-    console.log(computerChoice)
 
     if (computerChoiceInt <= 3) {
         computerChoiceString = "rock";
@@ -13,9 +12,7 @@ function getComputerChoice() {
     return computerChoiceString;
 }
 
-function singleGame() {
-    playerSelection = prompt("Type rock, paper or scissor: ");
-    playerSelection.toLowerCase();
+function singleGame(playerSelection) {
     if (playerSelection == "rock" || playerSelection == "paper" || playerSelection == "scissor") {
         console.log("picked")
         computerChoice = getComputerChoice();
@@ -37,27 +34,50 @@ function singleGame() {
 
 }
 
-function game() {
-    scorePlayer = 0;
-    scoreComputer = 0;
-    for (let i = 0; i<=4; i++) {
-        tempScore = singleGame();
-        if (tempScore < 0) {
-            scoreComputer++;
-        } else if (tempScore > 0) {
-            scorePlayer++;
-        }
-        console.log("Your score: " + scorePlayer + ", computer his score: " + scoreComputer);
-    }
-    if (scorePlayer > scoreComputer) {
-        console.log("Congrats! You win!"); 
-    }
-    if (scorePlayer < scoreComputer) {
-        console.log("You lose. Try again!"); 
-    }
-    if (scorePlayer == scoreComputer) {
-        console.log("It's a tie"); 
-    }
+const playerText = document.getElementsByClassName("player");   
+const computerText = document.getElementsByClassName("computer");
+
+
+console.log(playerText.innerHTML)
+
+const rockBtn = document.getElementsByClassName("rock");
+const paperBtn = document.getElementsByClassName("paper");
+const scissorBtn = document.getElementsByClassName("scissor");
+
+rockBtn[0].addEventListener('click', test)
+
+function test() {
+    console.log("runned");
 }
 
-game()
+rockBtn[0].addEventListener("click", function() {game("rock");});
+paperBtn[0].addEventListener("click", function() {game("paper");});
+scissorBtn[0].addEventListener("click", function() {game("scissor");});
+
+scorePlayer = 0;
+scoreComputer = 0;
+
+function game(playerSelection) {
+    
+    
+    tempScore = singleGame(playerSelection);
+    if (tempScore < 0) {
+        scoreComputer++;
+    } else if (tempScore > 0) {
+        scorePlayer++;
+    }
+    playerText[0].innerHTML = "Player: " + scorePlayer;
+    computerText[0].innerHTML = "Computer: " + scoreComputer;
+
+    console.log("Your score: " + scorePlayer + ", computer his score: " + scoreComputer);
+    if (scorePlayer == 5) {
+        console.log("You win!")
+        scorePlayer = 0;
+        scoreComputer = 0;
+    }  else if (scoreComputer == 5) {
+        console.log("You lose")
+        scorePlayer = 0;
+        scoreComputer = 0;
+    }  
+}
+
