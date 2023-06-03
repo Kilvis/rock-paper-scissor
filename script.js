98d16f0b9da8a7602d29695913bae18c125cbf38
Kilvis <kyndtkilian2006@gmail.com>
2023-06-03 03:59:59 +0200

    added the singleGame function in script.js

--- script.js
+++ script.js
@@ -10,3 +10,20 @@ function getComputerChoice() {
     }
     return computerChoiceString;
 }
+
+function singleGame() {
+    playerSelection = prompt("Type rock, paper or scissor: ");
+    if (playerSelection == "rock" || playerSelection != "paper" || playerSelection != "s"cissor") {
+        playerSelection = prompt("Please type rock, paper or scissor: ");
+    }
+    playerSelection.toLowerCase();
+    computerChoice = getComputerChoice();
+    
+    if (playerSelection == computerChoice) {
+        console.log("It's a tie")
+    } else if ((playerSelection == "rock" && computerChoice == "scissor") || (playerSelection == "scissor" && computerChoice == "paper") || (playerSelection == "paper" && computerChoice == "rock")) {
+        console.log("Congrats! You win!");
+    } else if ((playerSelection == "scissor" && computerChoice == "rock") || (playerSelection == "paper" && computerChoice == "scissor") || (playerSelection == "rock" && computerChoice == "paper")) {
+        console.log("You lose. Try again.");
+    }
+}
